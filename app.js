fffe0d0cb7a5d0f691633c6d78ddf9df05697d60
omribalut <skozzz@gmail.com>
2021-08-18 02:09:16 +0300

    Created 2 New Files

---
+++ app.js
@@ -0,0 +1,5 @@
+function calculate() {
+    var first = document.getElementById ('num1')
+    var second = document.getElementById ('num2')
+     document.getElementById ('result').value = first.value + second.value
+}
